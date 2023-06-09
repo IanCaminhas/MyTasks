b827dfced26855f942782dfa839b2186f1cc442b
IanPC <iancaminhas25@hotmail.com>
2023-06-09 08:21:20 -0300

    Criando o contexto TasksContext

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import {Home} from './src/pages/Home';
+import {TasksContent} from './src/context/tasksContent';
 
 const App = () => {
-  return <Home />;
+  /*
+      Colocamos o provider juntamente com o componente APP, pois APP é o primeiro a ser renderizado. APP é o primeiro a ser renderizado, sendo que tudo começa a partir de dele.
+      Configuramos o provider do nosso contexto no componente APP. No provider precisamos passar a propriedade value.
+      Na propriedade value é que vamos colocar a informação que queremos disponibilizar globalmente.
+      Aí o componente <Home/> vai ter acesso
+  */
+  return (
+    <TasksContent.Provider value={{id: '1', title: 'Task01'}}>
+      <Home />
+    </TasksContent.Provider>
+  );
 };
 
 export default App;
---
+++ src/context/TasksContent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//O tipo de informação que vai ter o contexto
+export interface ITasksContext {
+  id: string;
+  title: string;
+}
+
+/*
+Colocamos {} como default value...O typescript não suporta. Como contormar isso ?
+falando que {} é do tipo ITasksContext... Isso aqui é para forçar a barra
+*/
+export const TasksContent = React.createContext<ITasksContext>(
+  {} as ITasksContext,
+);
