75b1419f3512f1699026cae104d24dca21dbccd5
IanPC <iancaminhas25@hotmail.com>
2023-06-09 09:10:44 -0300

    Refatorando o contexto TasksContext

--- App.tsx
+++ App.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import {Home} from './src/pages/Home';
-import {TasksContext} from './src/context/TasksContext';
+import {TasksProvider} from './src/context/TasksContext';
 
 const App = () => {
   /*
       Colocamos o provider juntamente com o componente APP, pois APP é o primeiro a ser renderizado. APP é o primeiro a ser renderizado, sendo que tudo começa a partir de dele.
       Configuramos o provider do nosso contexto no componente APP. No provider precisamos passar a propriedade value.
       Na propriedade value é que vamos colocar a informação que queremos disponibilizar globalmente.
-      Aí o componente <Home/> vai ter acesso
+      Aí o componente <Home/> vai ter acesso.
+
+      Esse TasksContext.Provider fica engessado. O que foi feito:
+      Peguei o TasksContext.Provider value={{id: '1', title: 'Task01'} e coloquei dentro de TasksContext
+
+      <TasksContext.Provider value={{id: '1', title: 'Task01'}}>
+        <Home />
+      </TasksContext.Provider>
+
+      Como ficou:
+          <TasksProvider>
+             <Home />
+          </TasksProvider>
+          Agora APP não tem contato com o ContextProvider. Isolei o TasksProvider
+  );
+
   */
   return (
-    <TasksContext.Provider value={{id: '1', title: 'Task01'}}>
+    <TasksProvider>
       <Home />
-    </TasksContext.Provider>
+    </TasksProvider>
   );
 };
 
--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+interface IProps {
+  children: React.ReactElement;
+}
+
 //O tipo de informação que vai ter o contexto
 export interface ITasksContext {
   id: string;
@@ -13,3 +17,16 @@ falando que {} é do tipo ITasksContext... Isso aqui é para forçar a barra
 export const TasksContext = React.createContext<ITasksContext>(
   {} as ITasksContext,
 );
+
+export const TasksProvider: React.FunctionComponent<IProps> = ({
+  children,
+}: IProps) => {
+  /*children são os componentes filhos recebidos via props... Assim, os outros componentes não
+  tem acesso às implementações do Context.
+  */
+  return (
+    <TasksContext.Provider value={{id: '1', title: 'Task01'}}>
+      {children}
+    </TasksContext.Provider>
+  );
+};
